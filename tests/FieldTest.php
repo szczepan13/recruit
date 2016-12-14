f6cc76bb93685056e15c312b83d7fa784e2a61e8
Szczepan Ślęzak <szczepan13@gmail.com>
2016-12-14 23:00:16 +0100

    some tests

--- tests/FieldTest.php
+++ tests/FieldTest.php
@@ -8,6 +8,7 @@ class FieldTest extends TestCase
         $field = new Field(Field::CLOSED);
         $this->assertEquals($field->isClosed(), true);
         $this->assertEquals($field->isOpen(), false);
+        $this->assertEquals($field->isWin(), false);
     }
 
     public function testOpen()
@@ -15,6 +16,7 @@ class FieldTest extends TestCase
         $field = new Field(Field::OPEN);
         $this->assertEquals($field->isOpen(), true);
         $this->assertEquals($field->isClosed(), false);
+        $this->assertEquals($field->isWin(), false);
     }
 
     public function testWinning()
